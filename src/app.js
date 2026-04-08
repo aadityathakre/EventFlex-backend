524f9b43f517ff205a5832590f8fa37534f922d9
aadityathakre <adityathakre976@gmail.com>
2026-04-08 18:51:24 +0530

    ..

--- src/app.js
+++ src/app.js
@@ -51,18 +51,6 @@ app.use(
   })
 );
 
-// Manual preflight handler
-app.options('/*', (req, res) => {
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
-    res.header('Access-Control-Allow-Credentials', 'true');
-  }
-  res.sendStatus(200);
-});
-
 // Security middlewares
 app.use(sanitizeInput);
 app.use(rateLimit(1000, 15 * 60 * 1000)); // 1000 requests per 15 minutes
